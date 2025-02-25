5b2f6fc4f3974666c4b91df51249f51eb1705599
dscater <victorgonzalo_as@hotmail.com>
2025-02-25 11:01:37 -0400

    Reduccion contenido mail solicitud mantenimiento

--- resources/views/mail/SolicitudAtencion.blade.php
+++ resources/views/mail/SolicitudAtencion.blade.php
@@ -33,34 +33,26 @@
     @if ($datos['estado'] == 'PENDIENTE')
         <h1>Solicitud de Atención</h1>
         <p>Señor(es) {{ $datos['cliente']->razon_social }} su solicitud se encuentra <b>Pendiente</b>.</p>
-        <p><strong>Cliente: </strong> {{ $datos['cliente']->razon_social }}</p>
-        <p><strong>Personal Técnico: </strong> {{ $datos['solicitud_atencion']->personal->full_name }}</p>
-        <p><strong>Descripción de atención: </strong> {{ $datos['solicitud_atencion']->descripcion }}</p>
-        <p><strong>Fecha de atención y hora de llegada: </strong> {{ $datos['solicitud_atencion']->fecha_hora_t }}</p>
-        <p><strong>Estado: </strong> {{ $datos['solicitud_atencion']->estado }}</p>
     @endif
 
     {{-- EN PROCESO --}}
     @if ($datos['estado'] == 'EN PROCESO')
         <h1>Solicitud de Atención</h1>
         <p>Señor(es) {{ $datos['cliente']->razon_social }} su solicitud se encuentra <b>En Proceso</b>.</p>
-        <p><strong>Cliente: </strong> {{ $datos['cliente']->razon_social }}</p>
-        <p><strong>Personal Técnico: </strong> {{ $datos['solicitud_atencion']->personal->full_name }}</p>
-        <p><strong>Descripción de atención: </strong> {{ $datos['solicitud_atencion']->descripcion }}</p>
-        <p><strong>Fecha de atención y hora de llegada: </strong> {{ $datos['solicitud_atencion']->fecha_hora_t }}</p>
-        <p><strong>Estado: </strong> {{ $datos['solicitud_atencion']->estado }}</p>
     @endif
 
     {{-- ATENDIDO --}}
     @if ($datos['estado'] == 'ATENDIDO')
         <h1>Solicitud de Atención</h1>
         <p>Señor(es) {{ $datos['cliente']->razon_social }} su solicitud se encuentra <b>Finalizada</b>.</p>
-        <p><strong>Cliente: </strong> {{ $datos['cliente']->razon_social }}</p>
-        <p><strong>Personal Técnico: </strong> {{ $datos['solicitud_atencion']->personal->full_name }}</p>
-        <p><strong>Descripción de atención: </strong> {{ $datos['solicitud_atencion']->descripcion }}</p>
-        <p><strong>Fecha de atención y hora de llegada: </strong> {{ $datos['solicitud_atencion']->fecha_hora_t }}</p>
-        <p><strong>Estado: </strong> {{ $datos['solicitud_atencion']->estado }}</p>
     @endif
+
+    {{-- CONTENIDO --}}
+    <p><strong>Cliente: </strong> {{ $datos['cliente']->razon_social }}</p>
+    <p><strong>Personal Técnico: </strong> {{ $datos['solicitud_atencion']->personal->full_name }}</p>
+    <p><strong>Descripción de atención: </strong> {{ $datos['solicitud_atencion']->descripcion }}</p>
+    <p><strong>Fecha de atención y hora de llegada: </strong> {{ $datos['solicitud_atencion']->fecha_hora_t }}</p>
+    <p><strong>Estado: </strong> {{ $datos['solicitud_atencion']->estado }}</p>
 </body>
 
 </html>
